d03db57496f9eb869361617f10a93fe5ca977c2d
sahooAbhipsa01 <143869720+sahooAbhipsa01@users.noreply.github.com>
2024-12-09 12:35:10 +0530

    Update Courses.java

--- lmsProject/src/main/java/models/Courses.java
+++ lmsProject/src/main/java/models/Courses.java
@@ -1,37 +1,44 @@
 package models;
 
 public class Courses {
-	private int id;
+    private int id;
     private String name;
-    private int instructorId;
-	public Courses(int id, String name, int instructorId) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.instructorId = instructorId;
-	}
-	public Courses() {}
-	@Override
-	public String toString() {
-		return "Courses [id=" + id + ", name=" + name + ", instructorId=" + instructorId + "]";
-	}
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getInstructorId() {
-		return instructorId;
-	}
-	public void setInstructorId(int instructorId) {
-		this.instructorId = instructorId;
-	}
+    private int trainerId;
 
+    public Courses(int id, String name, int trainerId) {
+        this.id = id;
+        this.name = name;
+        this.trainerId = trainerId;
+    }
+
+    public Courses() {}
+
+    @Override
+    public String toString() {
+        return "Courses [id=" + id + ", name=" + name + ", trainerId=" + trainerId + "]";
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTrainerId() {
+        return trainerId;
+    }
+
+    public void setTrainerId(int trainerId) {
+        this.trainerId = trainerId;
+    }
 }
